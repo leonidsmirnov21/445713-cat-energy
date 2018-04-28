1efa77a317c4d736ba6825fdd8b6df893aa9e312
Леонид <33185661+leonidsmirnov21@users.noreply.github.com>
2018-04-28 16:07:28 +0300

    rework mistakes and aligning of the main page

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,14 +1,17 @@
 var before = document.querySelector('.example-slider__item--before');
 var after = document.querySelector('.example-slider__item--after');
+var scaleMobile = document.querySelector('.example-slider__scale-inner');
 var beforeButton = document.querySelector('.example-slider__scale-before');
 var afterButton = document.querySelector('.example-slider__scale-after');
 
 afterButton.addEventListener('click', function() {
   before.classList.add('example-slider__item--none');
   after.classList.remove('example-slider__item--none');
+  scaleMobile.classList.add('example-slider__scale-inner--right');
 });
 
 beforeButton.addEventListener('click', function() {
   after.classList.add('example-slider__item--none');
   before.classList.remove('example-slider__item--none');
+  scaleMobile.classList.remove('example-slider__scale-inner--right');
 });
